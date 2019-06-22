e2745aeacb8069545050f94bb93c710aebcb120c
matthieu <mc@esokia.com>
2019-06-22 22:07:38 +0400

    add htaccess and form

--- src/Form/RegistrationSeekerType.php
+++ src/Form/RegistrationSeekerType.php
@@ -7,11 +7,12 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\EmailType;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\TextType;
 use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 
 class RegistrationSeekerType extends AbstractType
 {
@@ -42,7 +43,7 @@ class RegistrationSeekerType extends AbstractType
                  ]
             ])
             ->add('address', TextType::class,[
-                'label' => 'Firstname*',
+                'label' => 'Address*',
                 'constraints' => [
                     new NotBlank(),
                     new Regex(array('pattern' => "/(\<\w*)((\s\/\>)|(.*\<\/\w*\>))/", 'match' => false, 'message' => "This field is invalid")),
@@ -50,15 +51,15 @@ class RegistrationSeekerType extends AbstractType
                 ]
             ])
             ->add('contact', TextType::class,[
-                'label' => 'Firstname*',
+                'label' => 'Contact*',
                 'constraints' => [
                     new NotBlank(),
                     new Regex(array('pattern' => "/(\<\w*)((\s\/\>)|(.*\<\/\w*\>))/", 'match' => false, 'message' => "This field is invalid")),
                     new Length(array('max'=>100,'maxMessage'=>'Cannot contain more than 100 Caractere'))
                 ]
             ])
-            ->add('dob', TextType::class,[
-                'label' => 'Firstname*',
+            ->add('dob', DateType::class,[
+                'label' => 'Date of Birth*',
                 'constraints' => [
                     new NotBlank(),
                     new Regex(array('pattern' => "/(\<\w*)((\s\/\>)|(.*\<\/\w*\>))/", 'match' => false, 'message' => "This field is invalid")),
@@ -66,7 +67,7 @@ class RegistrationSeekerType extends AbstractType
                 ]
             ])
             ->add('hqa', TextType::class,[
-                'label' => 'Firstname*',
+                'label' => 'Highest Qualification acheived*',
                 'constraints' => [
                     new NotBlank(),
                     new Regex(array('pattern' => "/(\<\w*)((\s\/\>)|(.*\<\/\w*\>))/", 'match' => false, 'message' => "This field is invalid")),
@@ -74,7 +75,7 @@ class RegistrationSeekerType extends AbstractType
                 ]
             ])
             ->add('cv', TextType::class,[
-                'label' => 'Firstname*',
+                'label' => 'CV*',
                 'constraints' => [
                     new NotBlank(),
                     new Regex(array('pattern' => "/(\<\w*)((\s\/\>)|(.*\<\/\w*\>))/", 'match' => false, 'message' => "This field is invalid")),
